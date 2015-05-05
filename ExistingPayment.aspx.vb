d7dc4a4669751cf22424ee5743c2650f01b29517
nfowlie <nfowlie@uncc.edu>
2015-05-05 01:46:23 -0400

    Unbroke the payment selection page

--- ExistingPayment.aspx.vb
+++ ExistingPayment.aspx.vb
@@ -2,7 +2,10 @@
 Partial Class ExistingPayment
     Inherits System.Web.UI.Page
 
+    
     Protected Sub DropDownList1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles DropDownList1.SelectedIndexChanged
         Session("paymentID") = DropDownList1.SelectedValue
     End Sub
+
+
 End Class
